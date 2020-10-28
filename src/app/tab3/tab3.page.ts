2e33d3b4bfe572b434c191434920151d3ef68778
Julio Heredia <julio.heredia@hotmail.com>
2020-10-28 09:31:51 -0500

    prueba 1

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
 
   constructor() {}
 
+  ngOnInit() {
+    let numero = 10 ;
+    debugger;
+    numero = numero + 50 ;
+    console.log(numero);
+  }
 }
